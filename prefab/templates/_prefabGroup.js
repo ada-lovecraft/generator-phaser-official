b721730fc604719e22e53722bc786d78930a716d
Dani Ramírez <thebluebishop@gmail.com>
2014-09-17 16:48:50 +0200

    Basic group prefab template

--- prefab/templates/_prefabGroup.js
+++ prefab/templates/_prefabGroup.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var <%= _.capitalize(prefabName) %> = function(game, x, y, frame) {
-  Phaser.Sprite.call(this, game, x, y, '<%= prefabSpriteKey %>', frame);
+var <%= _.capitalize(prefabName) %> = function(game, parent) {
+  Phaser.Sprite.call(this, game, parent);
 
   // initialize your prefab here
   
 };
 
-<%= _.capitalize(prefabName) %>.prototype = Object.create(Phaser.Sprite.prototype);
+<%= _.capitalize(prefabName) %>.prototype = Object.create(Phaser.Group.prototype);
 <%= _.capitalize(prefabName) %>.prototype.constructor = <%= _.capitalize(prefabName) %>;
 
 <%= _.capitalize(prefabName) %>.prototype.update = function() {
