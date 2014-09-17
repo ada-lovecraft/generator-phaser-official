f2370e642f73aa29b5694aedfc80967d6753c910
Dani Ramírez <thebluebishop@gmail.com>
2014-09-17 16:45:29 +0200

    TileSprite prefab template

--- prefab/templates/_prefabTileSprite.js
+++ prefab/templates/_prefabTileSprite.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var <%= _.capitalize(prefabName) %> = function(game, x, y, frame) {
-  Phaser.Sprite.call(this, game, x, y, '<%= prefabSpriteKey %>', frame);
+var <%= _.capitalize(prefabName) %> = function(game, x, y, width, height) {
+  Phaser.TileSprite.call(this, game, x, y, width, height, '<%= prefabSpriteKey %>');
 
   // initialize your prefab here
   
 };
 
-<%= _.capitalize(prefabName) %>.prototype = Object.create(Phaser.Sprite.prototype);
+<%= _.capitalize(prefabName) %>.prototype = Object.create(Phaser.TileSprite.prototype);
 <%= _.capitalize(prefabName) %>.prototype.constructor = <%= _.capitalize(prefabName) %>;
 
 <%= _.capitalize(prefabName) %>.prototype.update = function() {
