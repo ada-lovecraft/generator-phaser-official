ae621287caa995ad028fa0ccb15b3fed57e2c51a
Dani Ramírez <thebluebishop@gmail.com>
2014-09-17 15:52:44 +0200

    prefabType ask added

--- prefab/index.js
+++ prefab/index.js
@@ -6,15 +6,29 @@ var yeoman = require('yeoman-generator');
 
 var PrefabGenerator = yeoman.generators.NamedBase.extend({
   init: function() {
-    console.log('Creating the new prefab:', this.name);
+    this.log('Creating the new prefab:', this.name);
   },
   askFor: function () {
     var done = this.async();
 
     // replace it with a short and sweet description of your generator
-    var prefabNamePattern = new RegExp(/^\S+$/)
+    var prefabNamePattern = new RegExp(/^\S+$/);
+    var prefabTypes = "stg";
 
-    var prompts = [{
+    var prompts = [
+        {
+          name: 'prefabType',
+          message: 'What is the type of your prefab ([s]prite, [t]ileSprite or [g]roup)?',
+          validate: function (input) {
+            if (input && input.length === 1 && prefabTypes.indexOf(input) > -1) {
+              return true;
+            } else {
+              return 'The type can be only s, t or g.';
+            }
+          },
+          default: 's';
+        },
+        {
         name: 'prefabName',
         message: 'What is the name of your prefab?',
         validate: function(input) {
@@ -36,6 +50,7 @@ var PrefabGenerator = yeoman.generators.NamedBase.extend({
     this.prompt(prompts, function (props) {
       this.prefabName = props.prefabName;
       this.prefabSpriteKey = props.prefabSpriteKey;
+      this.prefabType = props.prefabType;
       done();
     }.bind(this));
   },
